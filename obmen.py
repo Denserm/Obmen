e8942a34350b57b17f01896fc78ba015ebfc7f4e
Andrey <maslakan78@gmail.com>
2024-10-19 19:53:00 +0300

    Создана функция exchange для получения из интернета курсов валют

--- obmen.py
+++ obmen.py
@@ -4,6 +4,28 @@ import pprint
 from tkinter import *
 from tkinter import messagebox as mb
 
+from bottle import response
+
+
+def exchange():
+    code = entry.get()
+
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/usd')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание", "Введите код валюты!")
+
+
 
 
 
