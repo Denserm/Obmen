19557bca69ee19d264a802a5f5c059a47e0218b3
Andrey <maslakan78@gmail.com>
2024-10-19 21:17:21 +0300

    В метку добавили названия валют.

--- obmen.py
+++ obmen.py
@@ -4,8 +4,10 @@ from tkinter import *
 from tkinter import messagebox as mb
 from tkinter import ttk
 
-
-from bottle import response
+def udate_c_label(event):
+    code = combobox.get()
+    name = cur[code]
+    c_label.config(text=name)
 
 
 def exchange():
@@ -18,7 +20,8 @@ def exchange():
             data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за 1 доллар")
+                c_name = cur[code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {c_name} за 1 доллар")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена!")
         except Exception as e:
@@ -27,7 +30,18 @@ def exchange():
         mb.showwarning("Внимание", "Введите код валюты!")
 
 
-
+cur = {
+    'RUB': 'Российский рубль',
+    'EUR': 'Евро',
+    'GBP': 'Британский фунт стерлингов',
+    'JPY': 'Японская йена',
+    'CNY': 'Китайский юань',
+    'KZT': 'Казахский танге',
+    'UZS': 'Узбекский сум',
+    'CHF': 'Швейцарский франк',
+    'AED': 'Дирхам ОАЭ',
+    'CAD': 'Канадский доллар'
+}
 
 
 window = Tk()
@@ -35,12 +49,13 @@ window.title("Курсы обмена валют")
 window.geometry("360x180")
 
 Label(text="Выберите код валюты").pack(padx=10, pady=10)
-cur = ['RUB', 'EUR', 'GBP', 'JPY', 'CNY', 'KZT', 'UZS', 'CHF', 'AED', 'CAD']
-combobox = ttk.Combobox(values=cur)
+# cur = ['RUB', 'EUR', 'GBP', 'JPY', 'CNY', 'KZT', 'UZS', 'CHF', 'AED', 'CAD']
+combobox = ttk.Combobox(values=list(cur.keys()))
 combobox.pack(padx=10, pady=10)
+combobox.bind("<<ComboboxSelected>>", udate_c_label)
 
-# entry = Entry()
-# entry.pack(padx=10, pady=10)
+c_label = ttk.Label()
+c_label.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
 
